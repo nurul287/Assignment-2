a127da170e00eccae254ae049a115bed7efeac86
nurul287 <nurulalamarif2@gmail.com>
2024-01-18 16:18:00 +0600

    created interface and model for users.

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,36 @@
+import { Model } from 'mongoose';
+
+export interface IUserName {
+  firstName: string;
+  lastName: string;
+}
+
+export interface IUserAddress {
+  street: string;
+  city: string;
+  country: string;
+}
+export interface IUserOrder {
+  productName: string;
+  price: number;
+  quantity: number;
+}
+
+export interface IUser {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: IUserName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: IUserAddress;
+  isDeleted: boolean;
+  orders?: IUserOrder[];
+}
+
+// for creating static
+export interface UserModel extends Model<IUser> {
+  isUserExists: (id: string) => Promise<IUser | null>;
+}
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,139 @@
+import bcrypt from 'bcrypt';
+import { Schema, model } from 'mongoose';
+import config from '../../config';
+import {
+  IUser,
+  IUserAddress,
+  IUserName,
+  IUserOrder,
+  UserModel,
+} from './user.interface';
+
+const userNameSchema = new Schema<IUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    maxlength: [20, 'First Name cannot be more than 20 characters'],
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+    trim: true,
+  },
+});
+
+const userAddressSchema = new Schema<IUserAddress>({
+  street: {
+    type: String,
+    required: [true, 'Street name is required'],
+    trim: true,
+  },
+  city: {
+    type: String,
+    required: [true, 'City is required'],
+    trim: true,
+  },
+  country: {
+    type: String,
+    required: [true, 'Country is required'],
+    trim: true,
+  },
+});
+const userOrderSchema = new Schema<IUserOrder>({
+  productName: {
+    type: String,
+    required: [true, 'Product Name is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+  },
+});
+
+const userSchema = new Schema<IUser, UserModel>({
+  userId: {
+    type: Number,
+    required: [true, 'User ID is required'],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, 'User name is required'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    maxlength: [20, 'Password can not be more than 20'],
+  },
+
+  fullName: {
+    type: userNameSchema,
+    required: [true, 'Full name is required'],
+  },
+  age: { type: Number, required: true },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    trim: true,
+  },
+  hobbies: {
+    type: [String],
+    required: [true, 'Hobbies is required'],
+  },
+  address: {
+    type: userAddressSchema,
+    required: [true, 'Address is required'],
+  },
+  isActive: { type: Boolean, default: true },
+  orders: userOrderSchema,
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// pre save middleware/hook
+userSchema.pre('save', async function (next) {
+  //hashing password and save into DB
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+// post save middleware/hook
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+// Query Middleware
+userSchema.pre('find', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+userSchema.pre('findOne', function (next) {
+  this.find({ isDeleted: { $ne: true } });
+  next();
+});
+userSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
+  next();
+});
+
+// creating a custom static method
+userSchema.statics.isUserExists = async (id: string) => {
+  const existingUser = await User.findOne({ id });
+  return existingUser;
+};
+
+export const User = model<IUser, UserModel>('User', userSchema);
