12c18480a209b084925b35d0b852d420421be4dd
nurul287 <nurulalamarif2@gmail.com>
2024-01-18 17:35:18 +0600

    added validation for users.

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+const userNameValidationSchema = z.object({
+  firstName: z.string().min(1).trim(),
+  lastName: z.string().min(1).trim(),
+});
+
+const userAddressValidationSchema = z.object({
+  street: z.string().min(1),
+  city: z.string().min(1),
+  country: z.string().min(1),
+});
+
+const userOrderValidationSchema = z.object({
+  productName: z.string().min(1),
+  price: z.number(),
+  quantity: z.number(),
+});
+
+const studentValidationSchema = z.object({
+  userId: z.number(),
+  password: z.string().max(20),
+  username: z.string().min(1),
+  fullName: userNameValidationSchema,
+  age: z.number(),
+  email: z.string().email(),
+  hobbies: z.string().array(),
+  address: userAddressValidationSchema,
+  isActive: z.boolean().default(true),
+  orders: userOrderValidationSchema.optional(),
+  isDeleted: z.boolean().default(false),
+});
+
+export default studentValidationSchema;
