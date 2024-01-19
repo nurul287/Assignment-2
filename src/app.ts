59a31feeb50a823fabb2cf6337d396894ba2bd89
nurul287 <nurulalamarif2@gmail.com>
2024-01-19 12:46:02 +0600

    deployment completed.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import { UserRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 
@@ -8,5 +8,12 @@ app.use(cors());
 
 // application routes
 app.use('/api/users', UserRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'Welcome to Assignment-2 API project',
+  });
+});
 
 export default app;
